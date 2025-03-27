19afc5c26cc59708182aabb7e4429ed47fc72f56
Nishtha Aggarwal <145486833+nishthaaggarwal24@users.noreply.github.com>
2025-03-27 20:30:40 +0530

    Add utils/logger.js 1743087639651

---
+++ utils/logger.js
@@ -0,0 +1,30 @@
+const winston = require('winston');
+const { combine, timestamp, printf, colorize } = winston.format;
+
+const logFormat = printf(({ level, message, timestamp }) => {
+  return `${timestamp} [${level}]: ${message}`;
+});
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: combine(
+    colorize(),
+    timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+    logFormat
+  ),
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'logs/combined.log' })
+  ],
+  exceptionHandlers: [
+    new winston.transports.File({ filename: 'logs/exceptions.log' })
+  ]
+});
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (ex) => {
+  throw ex;
+});
+
+module.exports = logger;
